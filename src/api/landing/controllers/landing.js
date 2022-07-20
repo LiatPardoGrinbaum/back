9dd1313d5ccd0a981746867dc0199aea46329c02
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-07-20 19:10:21 +0300

    users, landings, contacts models after routes testing in postman

--- src/api/landing/controllers/landing.js
+++ src/api/landing/controllers/landing.js
@@ -1,9 +1,29 @@
-'use strict';
-
+"use strict";
+// const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 /**
  *  landing controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::landing.landing');
+module.exports = createCoreController("api::landing.landing");
+// module.exports = {
+//   /**
+//    * Create a record.
+//    *
+//    * @return {Object}
+//    */
+
+//   async create(ctx) {
+//     let entity;
+//     if (ctx.is("multipart")) {
+//       const { data, files } = parseMultipartData(ctx);
+//       data.author = ctx.state.user.id;
+//       entity = await strapi.services.article.create(data, { files });
+//     } else {
+//       ctx.request.body.author = ctx.state.user.id;
+//       entity = await strapi.services.article.create(ctx.request.body);
+//     }
+//     return sanitizeEntity(entity, { model: strapi.models.article });
+//   },
+// };
